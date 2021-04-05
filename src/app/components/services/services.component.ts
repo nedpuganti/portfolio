85c98c6624bd6b5ffbe87298b263d57eb02623d4
Naren Edpuganti <nedpuganti@cerashealth.com>
2021-04-05 16:03:50 -0400

    fix

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -14,6 +14,7 @@ export class ServicesComponent implements OnInit {
         'Css/Sass',
         'Bootstrap',
         'Javascript / jQuery / Typescript',
+        'Tailwind',
       ],
     },
     {
